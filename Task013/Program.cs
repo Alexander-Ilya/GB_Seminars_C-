7587903edecd4269165fbf4d994a16f4f9fbcddc
Alex <252459@gmail.com>
2022-11-03 22:00:34 +0300

    Add Task013 and Task015

---
+++ Task013/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает , что третьей цифры нет.
+
+int number = 1256;
+int amountDigitInNumber = AmountDigitInNumber(number);
+
+if (amountDigitInNumber >= 3)
+{
+    int result = number;
+    for (int i = amountDigitInNumber - 3; i > 0; i--)
+    {
+        //Console.WriteLine(number);
+        result = result / 10;
+       // Console.WriteLine(result);
+    }
+    int digitalThree = result % 10;
+    Console.WriteLine(digitalThree);
+}
+else
+{
+Console.WriteLine($"В числе {number} меньше трёх цифр");
+}
+
+
+
+
+int AmountDigitInNumber(int num)
+{
+    int res = 1;
+    while (true)
+    {
+        if (num / 10 == 0) break;
+        num = num / 10;
+        res++;
+    }
+    Console.WriteLine($"Количество цифр в числе {res}");
+    return res;
+}
---
+++ Task015/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] dayOfWeek = {"понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+Console.Write("Введите день недели: ");
+int numberDayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+if (numberDayOfWeek > 0 && numberDayOfWeek <= 7)
+{
+    if (numberDayOfWeek == 6 || numberDayOfWeek == 7)
+    {
+        Console.WriteLine($"День № {numberDayOfWeek}  {dayOfWeek[numberDayOfWeek-1]}  выходной");
+    }
+    else
+    {
+         Console.WriteLine($"День № {numberDayOfWeek}  {dayOfWeek[numberDayOfWeek-1]}  не выходной");
+    }
+}
+else
+{
+     Console.WriteLine($"День № {numberDayOfWeek}  не существует");
+}
